1367a817d090a8569f489a1f3ae92cf0c9e22581
wmom5 <Paul@Paul-PC.fios-router.home>
2018-03-18 10:24:27 -0400

    Simple Return method

---
+++ perfanal/singleperiod.py
@@ -0,0 +1,15 @@
+'''
+Created on Mar 18, 2018
+
+@author: Paul
+'''
+
+
+#Returns a percentage
+def singlePeriod(start_value,end_value):
+    try:
+        #could also use ((end_value/start_value)-1)*100
+        return (end_value-start_value)/start_value*100
+    except ZeroDivisionError:
+        raise ValueError("Must pass non zero start value")
+    
